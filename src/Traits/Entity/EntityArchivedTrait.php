d97762787cb2002a2e3f9f2a609d1709e720ec45
pixiekat <pixiekat@noreply.codeberg.org>
2025-04-17 14:28:54 -0400

    add entityarchivedtrait

---
+++ src/Traits/Entity/EntityArchivedTrait.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+namespace App\Traits\Entity;
+
+trait EntityArchivedTrait {
+
+  public function archive(): self {
+    $this->setArchivedAt(new \DateTimeImmutable());
+    return $this;
+  }
+
+  public function getArchivedAt(): ?\DateTimeInterface {
+    return $this->archivedAt;
+  }
+
+  public function isArchived(): bool {
+    return $this->archivedAt !== null;
+  }
+
+  public function setArchivedAt(?\DateTimeInterface $archivedAt): self {
+    $this->archivedAt = $archivedAt;
+
+    return $this;
+  }
+
+  public function unarchive(): self {
+    $this->setArchivedAt(null);
+    return $this;
+  }
+
+}
