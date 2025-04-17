2a88c8b6bd275d530511e47af021a88c447eace1
pixiekat <pixiekat@noreply.codeberg.org>
2025-04-17 18:32:03 -0400

    fix invalid name

--- src/DependencyInjection/SymfonyHelpersExtension.php
+++ src/DependencyInjection/SymfonyHelpersExtension.php
@@ -11,6 +11,6 @@ class SymfonyHelpersExtension extends Extension {
   
   public function load(array $configs, ContainerBuilder $container): void {
     $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../../config'));
-    $loader->load('services.yaml');
+    $loader->load('services.yml');
   }
 }
