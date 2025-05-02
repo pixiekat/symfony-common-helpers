05fbc5907be7f4355a0fe1e7005274dd5c11ff86
pixiekat <pixiekat@noreply.codeberg.org>
2025-05-02 10:28:31 -0400

    add a basevoter abstract class

---
+++ src/Security/Voter/BaseVoter.php
@@ -0,0 +1,49 @@
+<?php
+declare(strict_types=1);
+namespace Pixiekat\SymfonyHelpers\Security\Voter;
+
+use Symfony\Bundle\SecurityBundle\Security;
+use Pixiekat\SymfonyHelpers\Traits as PixieTraits;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+abstract class BaseVoter extends Voter {
+
+  public function __construct(
+    protected readonly Security $security,
+    protected readonly LoggerInterface $logger,
+  ) {  }
+
+  /**
+   * Checks if the user is anonymous.
+   *
+   * @return boolean
+   */
+  public function isAnonymous(): bool {
+    return !$this->security->isGranted('ROLE_USER') ?? false;
+  }
+
+  /**
+   * Checks if the user is authenticated.
+   *
+   * @return boolean
+   */
+  public function isAuthenticated(): bool {
+    return $this->security->isGranted('ROLE_USER') ?? false;
+  }
+
+  /**
+   * Gets a list of attributes that this voter supports.
+   *
+   * @return array
+   */
+  protected function getAttributes(): array {
+    $constants = (new \ReflectionClass($this))->getConstants();
+    return array_filter($constants, function ($key) {
+      return strpos($key, "ACCESS_") !== 0;
+    }, ARRAY_FILTER_USE_KEY);
+  }
+
+}
