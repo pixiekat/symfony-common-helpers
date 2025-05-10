a5de1ceb31fe6d1c1154c02f36726b19a4e6fb0d
pixiekat <pixiekat@noreply.codeberg.org>
2025-05-10 18:19:13 -0400

    add ban voter

---
+++ src/Security/Voter/BanVoter.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+namespace Pixiekat\SymfonyHelpers\Security\Voter;
+
+use Pixiekat\SymfonyHelpers\Entity;
+use Pixiekat\SymfonyHelpers\Interfaces;
+use Pixiekat\SymfonyHelpers\Security as PixieSecurity;
+use Pixiekat\SymfonyHelpers\Traits;
+use Psr\Log\LoggerInterface;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\Voter;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+
+final class RunVoter extends BaseVoter implements Interfaces\Security\Voter\RunVoterInterface {
+
+  use PixieTraits\Voter\VoterCRUDTrait;
+
+  protected function supports(string $attribute, mixed $subject): bool {
+    return in_array($attribute, $this->getAttributes()) && $subject instanceof Entity\Ban;
+  }
+
+  protected function voteOnAttribute(string $attribute, mixed $subject, TokenInterface $token): bool {
+    $user = $token->getUser();
+
+    if (!$user instanceof UserInterface) {
+      return false;
+    }
+
+    switch ($attribute) {
+      case self::BAN_ADD_BAN:
+      case self::BAN_LIST_BANS:
+      case self::BAN_REMOVE_BAN:
+      case self::BAN_VIEW_BAN:
+        return $this->isAdmin();
+        break;
+    }
+  }
+
+}
