6e23f0cb1878897e451494b2aec2913c69ecc1ac
pixiekat <pixiekat@noreply.codeberg.org>
2025-05-20 06:32:59 -0400

    adding migration for audit_log and bans

---
+++ migrations/Version20250520102453.php
@@ -0,0 +1,47 @@
+<?php
+declare(strict_types=1);
+namespace Pixiekat\SymfonyHelpers\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20250520102453 extends AbstractMigration {
+  public function getDescription(): string {
+    return 'Adding audit_logs and bans tables';
+  }
+
+  public function up(Schema $schema): void {
+    $table = $this->getTable('audit_logs');
+    if (!$table) {
+      $this->addSql('CREATE TABLE audit_logs (id INT AUTO_INCREMENT NOT NULL, action VARCHAR(255) NOT NULL, entity_type VARCHAR(255) NOT NULL, performed_by VARCHAR(255) NOT NULL, additional_data LONGTEXT DEFAULT NULL COMMENT \'(DC2Type:array)\', created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+    else {
+      $this->write('Table audit_logs already exists, skipping creation.');
+    }
+
+    $table = $this->getTable('bans');
+    if (!$table) {
+      $this->addSql('CREATE TABLE bans (id INT AUTO_INCREMENT NOT NULL, ip_address VARCHAR(255) NOT NULL, expires_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+    else {
+      $this->write('Table bans already exists, skipping creation.');
+    }
+  }
+
+  public function down(Schema $schema): void {
+    $table = $this->getTable('audit_logs');
+    if ($table) {
+      $this->addSql('DROP TABLE audit_logs');
+    }
+    else {
+      $this->write('Table audit_logs does not exist, skipping deletion.');
+    }
+    $table = $this->getTable('bans');
+    if ($table) {
+      $this->addSql('DROP TABLE bans');
+    }
+    else {
+      $this->write('Table bans does not exist, skipping deletion.');
+    }
+  }
+}
