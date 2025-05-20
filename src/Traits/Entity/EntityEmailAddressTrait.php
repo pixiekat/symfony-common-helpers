78953ec0b881bcb122ac29fc16d8756e94b37363
pixiekat <pixiekat@noreply.codeberg.org>
2025-05-20 06:50:49 -0400

    handle multiple versions of email property

--- src/Traits/Entity/EntityEmailAddressTrait.php
+++ src/Traits/Entity/EntityEmailAddressTrait.php
@@ -8,14 +8,31 @@ trait EntityEmailAddressTrait {
    * {@inheritdoc}
    */
   public function getEmailAddress(): ?string {
-    return $this->emailAddress;
+    if (property_exists($this, 'emailAddress')) {
+      return $this->emailAddress;
+    }
+    else if (property_exists($this, 'email')) {
+      return $this->email;
+    }
+    else {
+      throw new \RuntimeException('No property to get email address');
+    }
+    return null;
   }
 
   /**
    * {@inheritdoc}
    */
   public function setEmailAddress(string $emailAddress): static {
-    $this->emailAddress = $emailAddress;
+    if (property_exists($this, 'email')) {
+      $this->email = $emailAddress;
+    }
+    else if (property_exists($this, 'emailAddress')) {
+      $this->emailAddress = $emailAddress;
+    }
+    else {
+      throw new \RuntimeException('No property to set email address');
+    }
 
     return $this;
   }
