42faa359167a28abc8b31c278a6534a25f07ce0c
pixiekat <pixiekat@noreply.codeberg.org>
2025-05-23 12:47:36 -0400

    add baseentity

---
+++ src/Entity/BaseEntity.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+namespace Pixiekat\SymfonyHelpers\Entity;
+
+use Pixiekat\SymfonyHelpers\Traits\Entity as PixieTraits;
+use Doctrine\ORM\Mapping as ORM;
+
+class BaseEntity {
+}
